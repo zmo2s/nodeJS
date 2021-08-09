401a698812e0a81c7aefda57aa824ad628d612d3
zmo2s <41673755+zmo2s@users.noreply.github.com>
2021-08-09 15:21:36 +0200

    Update serverMinimum.js

--- serverMinimum.js
+++ serverMinimum.js
@@ -3,7 +3,9 @@
 const http = require("http");
 const port = 3000;
 const hostname = "127.0.0.1";
-const server = http.createServer((req,res) => { res.statusCode = 200});
+const server = http.createServer((req,res) => { 
+  res.statusCode(200);
+});
 server.listen(port,hostname, () => {
   console.log(`Server run on this url http://${hostname}:${port}`)
 });
